4ee7edcc42cbd716950492475054f728c039d54d
Jeff Huynh <huynh5851@gmail.com>
2021-01-24 11:37:10 -0600

    fix style on tools component

--- src/components/tools/Tools.js
+++ src/components/tools/Tools.js
@@ -12,90 +12,35 @@ function Tools() {
 
     return (
         <section className="my-tools" id="my-tools">
-            <h2 className="section__title section__title--tools">Tools I Use</h2>    
-                            
-            <h2 className="section__subtitle section__subtitle--tools">Languages</h2>   
-            <div className="tools">
-                <div className="tool">                    
+            <h2 className="section__title section__title--tools">Tools I Use</h2>                                
+            
+            <div className="tools-wrapper">
+                <h2 className="section__subtitle section__subtitle--tools">Languages</h2>   
+                <div className="tools tools-languages">
                     <img className="tool__img" src={java_logo} alt="java logo" />
-                    <h3 className="tool-title">Java</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
-                </div>
-                <div className="tool">
                     <img className="tool__img" src={python_logo} alt="python logo" />
-                    <h3 className="tool-title">Python3</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
-                </div>
-                <div className="tool">
                     <img className="tool__img" src={python_logo} alt="python logo" />
-                    <h3 className="tool-title">Python3</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
-                </div>
-                <div className="tool">
                     <img className="tool__img" src={python_logo} alt="python logo" />
-                    <h3 className="tool-title">Python3</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
                 </div>
             </div>
-            <h2 className="section__subtitle section__subtitle--tools">Databases</h2> 
-            <div className="tools">
-                <div className="tool">
+            <div className="tools-wrapper">
+                <h2 className="section__subtitle section__subtitle--tools">Databases</h2> 
+                <div className="tools tools-databases">
                     <img className="tool__img" src={mongoDB_logo} alt="mongodb logo" />
-                    <h3 className="tool-title">MongoDB</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
-                </div>
-                <div className="tool">
                     <img className="tool__img" src={postgres_logo} alt="postgres logo" />
-                    <h3 className="tool-title">PostgreSQL</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
-                </div>                                
-            </div>
-            <h2 className="section__subtitle section__subtitle--tools">Frameworks</h2> 
-            <div className="tools">                    
-                <div className="tool">
-                    <img className="tool__img" src={reactjs_logo} alt="reactjs logo" />
-                    <h3 className="tool-title">ReactJS</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
                 </div>
-                <div className="tool">
+            </div>
+            <div className="tools-wrapper">
+                <h2 className="section__subtitle section__subtitle--tools">Frameworks</h2> 
+                <div className="tools tools-frameworks">                    
+                    <img className="tool__img" src={reactjs_logo} alt="reactjs logo" />
                     <img className="tool__img" src={flask_logo} alt="flask logo" />
-                    <h3 className="tool-title">Flask</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
-                </div>               
-            </div>            
-            <h2 className="section__subtitle section__subtitle--tools">Frameworks</h2> 
-            <div className="tools">                    
-                <div className="tool">
+                </div>            
+            </div>
+            <div className="tools-wrapper">
+                <h2 className="section__subtitle section__subtitle--tools">Frameworks</h2> 
+                <div className="tools tools-programs">                    
                     <img className="tool__img" src={postman_logo} alt="postman logo" />
-                    <h3 className="tool-title">Postman</h3>
-                    <p className="tool-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                    consequat.</p>
                 </div>
             </div>
         </section>
